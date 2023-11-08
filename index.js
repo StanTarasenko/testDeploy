6b0e45b55c457884fefaf364e9a0b9add3e16f5d
Stan Tarasenko <stan.tarasenko1991@gmail.com>
2023-11-08 14:46:18 +0200

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
+const pool = require('./db');
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await pool.query('SELECT * FROM todos')
+    res.json(todos.rows)
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.listen(PORT);
