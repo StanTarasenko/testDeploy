d12fea7e804bed699bf80d842ce44b3952d0aa85
Stan Tarasenko <stan.tarasenko1991@gmail.com>
2023-11-08 17:46:14 +0200

    allow origin 1

--- index.js
+++ index.js
@@ -2,23 +2,12 @@ const express = require('express');
 const path = require('path');
 const pool = require('./db');
 const PORT = process.env.PORT || 5000;
-// const cors = require('cors');
 
 const app = express();
 
-// let corsOptions = {
-//   origin: 'http://localhost:5000',
-//   optionsSuccessStatus: 200
-// }
-
-// app.use(cors(corsOptions));
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  next();
-})
+app.use(cors({
+  origin: "http://localhost:5000"
+}));
 
 app.get('/todos', async (req, res) => {
   try {
